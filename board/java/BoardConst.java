beb8ed0ddbb4a51aec4a1b888654fc1702ba4afd
신병철 <mousecjf@gmail.com>
2022-03-01 09:06:48 +0900

    Delete board directory

--- board/java/BoardConst.java
+++
@@ -1,7 +0,0 @@
-package sbc.board.consts;
-
-public class BoardConst {
-	public static final int BOARD_PS = 3;
-	public static final int BOARD_CP = 1;
-	public static final int BOARD_PAGE_NUM = 5;
-}
--- board/java/FilePath.java
+++
@@ -1,5 +0,0 @@
-package sbc.file.fileset;
-
-public class FilePath {
-	public static final String FILE_STORE = "C:/kosmo/spring/fileUpload/tmp/";
-}
